2347c615e32bc44ff7a4d760e3ae1e1025495937
MyBuddyErick <eirik.daniels@gmail.com>
2023-09-10 21:58:40 +0200

    Clicking outside the sidebar closes it.

--- myscript.js
+++ myscript.js
@@ -2,14 +2,23 @@ document.addEventListener('DOMContentLoaded', function () {
     const sidebar = document.querySelector('.sidebar');
     const openButton = document.getElementById('open-sidebar');
     const closeButton = document.getElementById('close-sidebar');
+    const overlay = document.getElementById('overlay');
 
     // Function to open the sidebar
     openButton.addEventListener('click', function () {
         sidebar.style.left = '0';
+        overlay.style.display = 'block'; // Show the overlay
     });
 
     // Function to close the sidebar
     closeButton.addEventListener('click', function () {
         sidebar.style.left = '-100%';
+        overlay.style.display = 'none'; // Hide the overlay
+    });
+
+    // Function to close the sidebar when overlay is clicked
+    overlay.addEventListener('click', function () {
+        sidebar.style.left = '-100%';
+        overlay.style.display = 'none'; // Hide the overlay
     });
 });
